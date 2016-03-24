4b61c594f5744783a2fb1ded1f99c90e56a398be
Karl Czajkowski <karlcz@isi.edu>
2016-03-24 11:01:29 -0700

    fix spelling in ImageProperties.xml s/meterScaleInPixel/meterScaleinPixels/

--- pyramid/czi2dzi/czi2dzi.py
+++ pyramid/czi2dzi/czi2dzi.py
@@ -381,7 +381,7 @@ def metadata_to_xml(meta, channelno, channeldir):
 <IMAGE_PROPERTIES
     width="%(W)d"
     height="%(H)d"
-    meterScaleInPixel="%(PPM)f"
+    meterScaleInPixels="%(PPM)f"
     numTiles="%(NT)d"
     numImages="1"
     version="2.0"
